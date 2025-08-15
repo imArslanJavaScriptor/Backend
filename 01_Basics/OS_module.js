3237973c81062fd05dd07912a065843a76a8b062
Muhammad Arslan <imarslan7061@gmail.com>
2025-08-16 00:21:56 +0500

    01_Basics Update 1

--- 01_Basics/OS_module.js
+++
@@ -1,8 +0,0 @@
-const os = require("os")
-
-console.log(os)
-console.log('Platform', os.platform())
-console.log("User: ", os.userInfo())
-console.log("CPU Architecture:", os.arch())
-console.log('Free Memory:', os.freemem(), 'Bytes')
-console.log('Total Memory:', os.totalmem(), 'Bytes')
