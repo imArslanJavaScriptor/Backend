ee1f615997c834b39346815cbfabff998cf8842b
Arslan <imarslan7061@gmail.com>
2025-08-20 13:02:15 +0500

    Push Update Morning

--- 01_Basics/03_Crypto_Module.js
+++ 01_Basics/03_Crypto_Module.js
@@ -1,13 +1,38 @@
 const crypto = require("crypto");
 
-console.log(crypto.randomBytes(8).toString("hex"));
+// console.log(crypto.randomBytes(8).toString("hex"));
 
-const hashedValue = crypto.createHash("sha256").update("Arslan").digest("hex");
+// const hashedValue = crypto.createHash("sha256").update("Arslan").digest("hex");
 
-const confirmHashedValue = crypto
+// const confirmHashedValue = crypto
+//   .createHash("sha256")
+//   .update("Arslan")
+//   .digest("hex");
+
+// console.log(hashedValue);
+// console.log(confirmHashedValue);
+
+// ============= Register User ==============
+const password = "ThisIsMyPassword240";
+
+const hasedPassword = crypto
   .createHash("sha256")
-  .update("Arslan")
+  .update(password)
   .digest("hex");
 
-console.log(hashedValue);
-console.log(confirmHashedValue);
+console.log("Hashed Password: ", hasedPassword);
+
+// ============= Verify User ==============
+
+const enteredPassword = "ThisIsMyPassword240";
+
+const confirmHashedPassword = crypto
+  .createHash("sha256")
+  .update(enteredPassword)
+  .digest("hex");
+
+if (confirmHashedPassword === hasedPassword) {
+  console.log("User Verified Successfully");
+} else {
+  console.log("User Verification Failed");
+}
